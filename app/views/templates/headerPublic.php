163356ee47fac6d4196da4553ddaf499aa29861d
msami0511 <mohamashaikh@algomau.ca>
2025-07-18 23:42:16 +0000

    Public header file updated

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -1,20 +1,30 @@
-    <?php
-    if (isset($_SESSION['auth']) == 1) {
-        header('Location: /home');
-    }
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <link rel="icon" href="/favicon.png">
-        <title>COSC 4806</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="mobile-web-app-capable" content="yes">
-
-        <!-- Custom CSS (Bootstrap removed) -->
-        <link rel="stylesheet" href="/public/css/style.css">
-    </head>
-    <body>
+<?php
+if (isset($_SESSION['auth']) && $_SESSION['auth'] == 1) {
+    header('Location: /home');
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="/favicon.png">
+    <title>COSC 4806</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">COSC 4806</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#publicNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="publicNavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+        <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
